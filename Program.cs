7d3aadd77d345b3c9a098546952cdbaae1ff632d
Felipe Marini <felipemarini71188@gmail.com>
2020-11-18 12:06:58 -0300

    Multiplication Table done

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Multiplication_Table_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplication Table");
+            Console.WriteLine("Insert a number (integer) to know its multiplication table (from 0 to 10): ");
+            int numero = Convert.ToInt16(Console.ReadLine());
+
+            for (int num = 0; num<11; num++)
+            {
+
+                int resultado = num*numero;
+                Console.WriteLine($"{num} x {numero} = {resultado}");
+
+
+
+            }
+
+        }
+    }
+}

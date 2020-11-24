1761af7252f888502bea707a291bd58e290bdefe
Felipe Marini <felipemarini71188@gmail.com>
2020-11-24 12:23:53 -0300

    Algorithm with few changes remade

--- Program.cs
+++ Program.cs
@@ -6,16 +6,18 @@ namespace Multiplication_Table_Algorithm
     {
         static void Main(string[] args)
         {
+            Console.WriteLine();
             Console.WriteLine("Multiplication Table");
-            Console.WriteLine("Insert a number (integer) to know its multiplication table (from 0 to 10): ");
+            Console.WriteLine();
+            Console.WriteLine("Insert a number (integer) to know its multiplication table (from 1 to 10):\n ");
             int numero = Convert.ToInt16(Console.ReadLine());
 
             for (int num = 0; num<11; num++)
             {
 
                 int resultado = num*numero;
+                Console.WriteLine();
                 Console.WriteLine($"{num} x {numero} = {resultado}");
-
 
 
             }
@@ -23,3 +25,4 @@ namespace Multiplication_Table_Algorithm
         }
     }
 }
+
